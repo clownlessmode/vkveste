3318ec379b5bf892a72e822eddf9c35cbb7cb71c
Kovalenko Rodion <167234046+clownlessmode@users.noreply.github.com>
2025-01-12 11:37:54 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  remotePatterns: [
+    {
+      protocol: "http",
+      hostname: "89.104.69.151",
+    },
+  ],
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ["tvr.mir-kvestov.ru", "localhost"],
+    domains: ["tvr.mir-kvestov.ru", "localhost", "89.104.69.151"],
   },
 }
 
