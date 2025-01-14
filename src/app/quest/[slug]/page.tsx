2893a89cf510802b1fc6b0a01ddb79e301258a3f
Kovalenko Rodion <167234046+clownlessmode@users.noreply.github.com>
2025-01-15 01:38:46 +0700

    fix

--- src/app/quest/[slug]/page.tsx
+++ src/app/quest/[slug]/page.tsx
@@ -271,7 +271,7 @@ export default async function QuestPage({ params }: QuestPageProps) {
                     height={1200}
                     quality={100}
                   />
-                  <div className="absolute inset-0 bg-black/30" />
+                  <div className="absolute inset-0 bg-black/50" />
                 </div>
               </div>
             </div>
